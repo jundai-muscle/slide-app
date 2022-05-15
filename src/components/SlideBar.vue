90d8a29ea7bd58a58536fbb61a6758350bb4bfbb
jundai-muscle <junsofteni0817@icloud.com>
2022-05-15 23:08:41 +0900

    0515

---
+++ src/components/SlideBar.vue
@@ -0,0 +1,67 @@
+<template>
+<div  id="sidebar" style="position: fixed">
+    <v-card height="100%" width="200" class="mx-auto">>
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        max-height
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-content >
+            <v-list-item-title class="text-h5" >
+                <router-link :to="{path:item.link}" 
+                style="color:brack;text-decoration:none">
+                    {{ item.title }}
+                </router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+            {title:'Home',link:'/'},
+            {title:'Now Playing',link:'/nowplaying'},
+            { title: 'Favorite', link: '/favorite' },
+            { title: 'Interest', link: '/interest' },
+          //{ title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
+
+<style scoped>
+#sidebar{
+    position:absolute;
+    left:0%;
+    height:100%;
+    z-index:10;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,26 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { initializeApp } from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyAJzCmC68GeoIQU09jWaHG0JmN94RNSUao",
+  authDomain: "login-3ad54.firebaseapp.com",
+  projectId: "login-3ad54",
+  storageBucket: "login-3ad54.appspot.com",
+  messagingSenderId: "294870895007",
+  appId: "1:294870895007:web:21a68d3b765cdb5b0755aa",
+  measurementId: "G-RVZPG4C43D",
+};
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+initializeApp(config);
